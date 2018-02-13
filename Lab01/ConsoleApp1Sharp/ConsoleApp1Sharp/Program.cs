f21bfaf284c055873cb65ce6c68603a6ed200c88
zemla4ok <dima-vital@live.com>
2018-02-14 00:00:22 +0300

    first lab OS

---
+++ Lab01/ConsoleApp1Sharp/ConsoleApp1Sharp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp1Sharp
+{
+    class Program
+    {
+        [DllImport("Dll3.dll", EntryPoint = "AddNumbers")]
+        public static extern int AddNumbers(int x, int y);
+
+        [DllImport("Dll3.dll", EntryPoint = "GetMyCurrentTime")]
+        public static extern void GetMyCurrentTime();
+
+        [DllImport("user32.dll")]
+        public static extern int MessageBox(int hWnd, String pText, String pCaption, int utype);
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            String pText = "Hello, programmer";
+            String pCaption = "See dll in action";
+            MessageBox(0, pText, pCaption, 0);
+            Console.WriteLine("Sum of 2 and 3 is {0}", AddNumbers(2, 3));
+
+            GetMyCurrentTime();
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Lab01/ConsoleApplication1/ConsoleApplication1/dll.h
@@ -0,0 +1,8 @@
+#pragma once
+
+void LetterList();
+int PutInt(int param);
+
+void PrintToFile(char* file_name, char* str);
+void StartProcess(wchar_t* path_to_process);
+bool CheckFile(char* directory, char* file_name);
